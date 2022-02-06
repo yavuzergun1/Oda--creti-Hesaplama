211adb1dd15c4d290cd5b740835f9a97aac9f739
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-02-06 20:58:05 +0300

    kayit

--- deneme.js
+++
@@ -1,7 +0,0 @@
-// const date1 = yazilanYatisTarihi.textContent;
-// const date2 = taburcuTarihi.textContent;
-// const diffTime = Math.abs(taburcuTarihi.value - yatisTarihi.value);
-// const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
-// // console.log(diffTime + " milliseconds");
-// console.log(diffDays + " days");
-
