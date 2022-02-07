b6e40415dd7b2491ef29598fbe37203913329d82
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-02-07 12:42:02 +0300

    button

--- script.js
+++ script.js
@@ -11,24 +11,59 @@ let yatisTarihi= document.querySelector(".tarih-yatis");
 let taburcuTarihi= document.querySelector(".tarih-taburcu");
 let yazilanYatisTarihi= document.querySelector(".p-tarih-yatis-yaz");
 let yazilanTaburcuTarihi= document.querySelector(".p-tarih-taburcu-yaz");
-let yatisSubmit= document.querySelector(".yatis-submit");
-let taburcuSubmit= document.querySelector(".taburcu-submit");
 let kalinanGunSayisi= document.querySelector(".gun-sayisi");
-let kalinanGunSubmit= document.querySelector(".kalinanGunSubmit");
+let buttonSubmit= document.querySelector(".buttonSubmit");
+let buttonHesapla= document.querySelector(".hesapla")
 let yazTutar= document.querySelector(".p-tutar-yaz")
 
 
 
-yatisSubmit.addEventListener('click', yatisYaz);
+buttonSubmit.addEventListener('click', yatisYaz);
 
 function yatisYaz(){
     yazilanYatisTarihi.textContent= yatisTarihi.value;
-    
-    taburcuSubmit.addEventListener('click', taburcuYaz);
-    function taburcuYaz(){
-        yazilanTaburcuTarihi.textContent= taburcuTarihi.value;
-        // console.log(taburcuTarihi.value);
+    yazilanTaburcuTarihi.textContent= taburcuTarihi.value;
+    girilenOda.textContent= `${secilenBlok.value}-${secilenOda.value}`;
+    for (let i=0; i<ozelOdalar.length; i++){
+        if (girilenOda.textContent == yariOzelOdalar[i]){
+            odaTipi.textContent= 'YARI ÖZEL';
+            gunlukTutarYaz.textContent= '49 TL'
+            buttonHesapla.addEventListener('click', kalinanGunYaz);
+    function kalinanGunYaz(){
+        console.log(`Kalınan Gün Sayısı: ${kalinanGunSayisi.value}`);
+        let tutar= kalinanGunSayisi.value*49;
+        yazTutar.textContent= `${tutar}TL`
+        // console.log(`Tutar: ${tutar}TL`);
+        
+        }
     }
+        if (girilenOda.textContent == ozelOdalar[i]){
+            odaTipi.textContent= 'ÖZEL';
+            gunlukTutarYaz.textContent= '99 TL'
+            buttonSubmit.addEventListener('click', kalinanGunYaz);
+            function kalinanGunYaz(){
+                // console.log(`Kalınan Gün Sayısı: ${kalinanGunSayisi.value}`);
+                let tutar= kalinanGunSayisi.value*99;
+                yazTutar.textContent= `${tutar}TL`
+                // console.log(`Tutar: ${tutar}TL`);
+                
+                }
+        }
+        if (girilenOda.textContent == genelOdalar[i]){
+            odaTipi.textContent= 'GENEL ';
+            gunlukTutarYaz.textContent= 'ÜCRETSİZ';
+            buttonSubmit.addEventListener('click', kalinanGunYaz);
+            function kalinanGunYaz(){
+                yazTutar.textContent= "Ücretsiz"
+                
+                }
+        }
+    }
+    // taburcuSubmit.addEventListener('click', taburcuYaz);
+    // function yatisYaz(){
+    //     yazilanTaburcuTarihi.textContent= taburcuTarihi.value;
+    //     // console.log(taburcuTarihi.value);
+    // }
 }
 
 
@@ -36,43 +71,9 @@ function yatisYaz(){
 
 secilenOda.addEventListener('click', odaYaz);
 function odaYaz() {
-girilenOda.textContent= `${secilenBlok.value}-${secilenOda.value}`;
 
-for (let i=0; i<ozelOdalar.length; i++){
-    if (girilenOda.textContent == yariOzelOdalar[i]){
-        odaTipi.textContent= 'YARI ÖZEL';
-        gunlukTutarYaz.textContent= '49 TL'
-        kalinanGunSubmit.addEventListener('click', kalinanGunYaz);
-function kalinanGunYaz(){
-    console.log(`Kalınan Gün Sayısı: ${kalinanGunSayisi.value}`);
-    let tutar= kalinanGunSayisi.value*49;
-    yazTutar.textContent= `${tutar}TL`
-    // console.log(`Tutar: ${tutar}TL`);
-    
-    }
-}
-    if (girilenOda.textContent == ozelOdalar[i]){
-        odaTipi.textContent= 'ÖZEL';
-        gunlukTutarYaz.textContent= '99 TL'
-        kalinanGunSubmit.addEventListener('click', kalinanGunYaz);
-        function kalinanGunYaz(){
-            // console.log(`Kalınan Gün Sayısı: ${kalinanGunSayisi.value}`);
-            let tutar= kalinanGunSayisi.value*99;
-            yazTutar.textContent= `${tutar}TL`
-            // console.log(`Tutar: ${tutar}TL`);
-            
-            }
-    }
-    if (girilenOda.textContent == genelOdalar[i]){
-        odaTipi.textContent= 'GENEL ';
-        gunlukTutarYaz.textContent= 'ÜCRETSİZ';
-        kalinanGunSubmit.addEventListener('click', kalinanGunYaz);
-        function kalinanGunYaz(){
-            yazTutar.textContent= "Ücretsiz"
-            
-            }
-    }
-}
+
+
 }
 
 // TARİH SEÇİM İŞLEMİ
